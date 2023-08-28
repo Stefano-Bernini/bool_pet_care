e6b03360bdfbecdf6b85553ec5bc8b2564402774
michelecanini <studio.michelecanini@gmail.com>
2023-08-28 22:23:49 +0200

    added button dashboard index

--- resources/views/admin/animals/index.blade.php
+++ resources/views/admin/animals/index.blade.php
@@ -7,6 +7,7 @@
                 <h1>I nostri animali</h1>
                 <div class="btn-container">
                     <a href="{{ Route('admin.animals.create') }}"><button class="btn btn-success">Aggiungi Animale</button></a>
+                    <a href="{{ Route('admin.dashboard') }}"><button class="btn btn-dark">Dashboard</button></a>
                 </div>
             </div>
             <div class="col-12">
