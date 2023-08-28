acdfcc11658ca55afc2c948c813f8ae1e417a9c8
ChristianBianchi99 <bianchi848@gmail.com>
2023-08-28 17:28:58 +0200

    Spostato controller vaccine

--- app/Http/Controllers/Admin/VaccineController.php
+++ app/Http/Controllers/Admin/VaccineController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\Vaccine;
 use App\Http\Requests\StoreVaccineRequest;
 use App\Http\Requests\UpdateVaccineRequest;
